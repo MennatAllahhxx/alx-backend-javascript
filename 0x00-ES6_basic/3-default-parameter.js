1f73dd533a12cbf926b8c4c32501cb38d6f7b7f8
Menna Hassan <h.menna85@gmail.com>
2024-03-05 02:53:18 +0200

    task 3 updated

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,8 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  // eslint-disable-next-line no-param-reassign
-  if (expansion1989 === undefined) expansion1989 = 89;
-  // eslint-disable-next-line no-param-reassign
-  if (expansion2019 === undefined) expansion2019 = 19;
-
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
