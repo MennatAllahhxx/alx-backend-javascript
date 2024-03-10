3fe9def9fd6520a872147599f88d3b0fcf6fa520
Menna Hassan <h.menna85@gmail.com>
2024-03-10 10:43:20 +0200

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,6 +7,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled([p1, p2]).then((ress) => ress.map((res) => ({
     status: res.status,
-    value: res.value,
+    value: res.status === 'fulfilled' ? res.value : String(res.reason),
   })));
 }
