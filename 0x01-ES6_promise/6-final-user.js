9d4362a1af389c847099f58f7d81573106eaf0e4
Menna Hassan <h.menna85@gmail.com>
2024-03-10 10:38:57 +0200

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,10 +5,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const p1 = signUpUser(firstName, lastName);
   const p2 = uploadPhoto(fileName);
 
-  return Promise.allSettled([p1, p2]).then((ress) => {
-    return ress.map((res) => ({
-      status: res.status,
-      value: res.value,
-    }));
-  });
+  return Promise.allSettled([p1, p2]).then((ress) => ress.map((res) => ({
+    status: res.status,
+    value: res.value,
+  })));
 }
