c9ee7eacb825ddd8b1bb803917fb538c8c29393d
Menna Hassan <h.menna85@gmail.com>
2024-03-10 12:19:35 +0200

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (e) {
-    queue.push(e);
+    queue.push(String(e));
   } finally {
     queue.push('Guardrail was processed');
   }
