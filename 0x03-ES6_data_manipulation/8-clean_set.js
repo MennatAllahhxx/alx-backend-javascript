9d9ada8246986e7f1b110225fe8e3a0cad4a36b6
Menna Hassan <h.menna85@gmail.com>
2024-03-13 07:36:13 +0200

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if ((startString.length === 0) || (typeof startString != 'string')) {
+  if ((startString.length === 0) || (typeof startString !== 'string')) {
     return '';
   }
   return [...set].filter((value) => value.startsWith(startString, 0)).map((value) => value.slice(startString.length)).join('-');
