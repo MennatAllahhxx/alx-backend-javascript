14a7e13da87017f632b758b84bd1c9e5e603988d
Menna Hassan <h.menna85@gmail.com>
2024-03-13 07:25:21 +0200

    task 11

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+const weakMap = new WeakMap();
+function queryAPI(endpoint) {
+    if (!(weakMap.has(endpoint))) {
+        weakMap.set(endpoint, 0);
+    }
+
+    const count = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, count);
+
+    if (count >= 4) {
+        throw new Error('Endpoint load is high');
+    }
+}
+export {weakMap, queryAPI};
