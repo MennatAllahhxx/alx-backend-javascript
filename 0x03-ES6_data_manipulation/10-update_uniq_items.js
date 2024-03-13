49422819d7c6160b1b9e9d28b59385fb19132ce5
Menna Hassan <h.menna85@gmail.com>
2024-03-13 06:28:47 +0200

    task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 export default function updateUniqueItems(map) {
-    const newMap = map.forEach((key, value) => {
+    const newMap = map.forEach((value, key) => {
         if (value === 1) {
-            newMap.set(key, 10);
+            map.set(key, 100);
         }
     });
     return newMap;
